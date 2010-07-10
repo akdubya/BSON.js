416798b6ce2559c5be9f6f0c54ebe6147aff24e6
akdubya <alekswilliams@earthlink.net>
2010-07-10 17:33:10 -0400

    Minor: cleanup test setup

--- test/common.js
+++ test/common.js
@@ -5,7 +5,6 @@ exports = module.exports = global;
 exports.testDir = path.dirname(__filename);
 exports.fixturesDir = path.join(exports.testDir, "fixtures");
 exports.libDir = path.join(exports.testDir, "../lib");
-exports.PORT = 12346;
 
 require.paths.push(exports.libDir);
 
